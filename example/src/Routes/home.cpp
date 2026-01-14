0b86b4166ebc41511247e2b65c6bb8a7b1f4cc5b
tutoraddicts <rohitdutta907@gmail.com>
2026-01-15 03:35:45 +0530

    added new theme

--- example/src/Routes/home.cpp
+++ example/src/Routes/home.cpp
@@ -3,5 +3,5 @@
 #include "../../vendor/agai/include/v1/server/routing/routing.h"
 
 auto Home = Agai::Get("/", [](const Agai::HttpRequest &req) -> Agai::Response {
-  return Agai::View("home");
+  return Agai::View("themes.atlas-portfolio.home");
 });

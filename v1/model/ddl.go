64b2620c12481036c9dc3eee319f64b7a6d86263
pritam-is-next <rohitdutta907@gmail.com>
2026-01-25 19:55:52 +0530

    fix primary key contrain issue

--- v1/model/ddl.go
+++ v1/model/ddl.go
@@ -31,16 +31,18 @@ func (m *meta) addField(field *Field) {
 // function to change the field details
 func (m *meta) modifyDBField(field *Field) {
 	// ALTER TABLE `users` CHANGE `userId` `userId` INT(30) NOT NULL AUTO_INCREMENT;
-	response := "ALTER TABLE `" + m.TableName + "` "
-	response += "CHANGE `" + field.name + "` " + field.columnDefinition() + ";"
+	response := "ALTER TABLE `" + m.TableName + "`"
+	//DROP FOREIGN KEY IF EXISTS `fk_Users_Id`;
+	response += " DROP FOREIGN KEY IF EXISTS `fk_" + field.table_name + "_" + field.name + "`,\n"
+	response += " CHANGE `" + field.name + "` " + field.columnDefinition() + ";"
 
 	if databaseObj, err := DatabaseHandler.GetDatabase(); err != nil {
-		panic("Error While Changing Field" + err.Error())
+		panic("\nError While Changing Field" + err.Error())
 	} else {
 		if _, sql_err := databaseObj.Exec(response); sql_err != nil {
-			panic("Error While Changing the Table Field" + sql_err.Error() + "SQL queryBuilder: " + response)
+			panic("\nError While Changing the Table Field" + sql_err.Error() + "\nSQL queryBuilder: " + response)
 		} else {
-			log.Info("[modifyDBField]   Table: %-20s | Field Updated: %-20s\n", m.TableName, field.name)
+			log.Info("\n[modifyDBField]   Table: %-20s | Field Updated: %-20s\n", m.TableName, field.name)
 		}
 	}
 }
@@ -48,14 +50,16 @@ func (m *meta) modifyDBField(field *Field) {
 // Drop a field from the databasess
 func (m *meta) removeDBField(fieldName string) {
 	//ALTER TABLE `users` DROP `userId`;
-	queryBuilder := "ALTER TABLE `" + m.TableName + "` DROP `" + fieldName + "`;"
+	queryBuilder := "ALTER TABLE `" + m.TableName + "`"
+	queryBuilder += " DROP FOREIGN KEY IF EXISTS `fk_" + m.TableName + "_" + fieldName + "`,\n"
+	queryBuilder += " DROP `" + fieldName + "`;"
 	if databaseObj, err := DatabaseHandler.GetDatabase(); err != nil {
-		panic("Error While Deleting Field" + err.Error())
+		panic(fmt.Sprintf("\nError While Deleting Field : %s\n queryBuilder: %s", err.Error(), queryBuilder))
 	} else {
 		if _, sql_err := databaseObj.Exec(queryBuilder); sql_err != nil {
-			panic("Error While Deleting the Field" + sql_err.Error())
+			panic(fmt.Sprintf("\nError While Deleting the Field: %s\n queryBuilder: %s", sql_err.Error(), queryBuilder))
 		} else {
-			fmt.Printf("[removeDBField]     Table: %-20s | Field Dropped: %-20s\n", m.TableName, fieldName)
+			fmt.Printf("\n[removeDBField]     Table: %-20s | Field Dropped: %-20s\n", m.TableName, fieldName)
 		}
 	}
 }
